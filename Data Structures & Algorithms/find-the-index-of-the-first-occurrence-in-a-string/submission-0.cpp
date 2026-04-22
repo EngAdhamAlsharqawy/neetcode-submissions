49b31ba3a6dc62d76307ff19ab9002cbcd71e8a6
Adham Al-Sharqawy <eng.adham.alsharqawy@gmail.com>
2026-04-22 21:32:52 +0200

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for (int i{}; i < haystack.size(); ++i) {
+            if (haystack[i] == needle[0]) {
+                int j{ 1 };
+                for (; j < needle.size(); ++j)
+                    if (haystack[i + j] != needle[j])
+                        break;
+                if (j == needle.size())
+                    return i;
+            }
+        }
+        return -1;
+    }
+};
