fa969b4c8e61f3cb8bf81f7345744991788bcc61
Adham Al-Sharqawy <eng.adham.alsharqawy@gmail.com>
2026-05-13 02:42:44 +0300

    Add: monotonic-array - submission-3

---
+++ Data Structures & Algorithms/monotonic-array/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        if (nums[0] <= nums[n - 1]) {
+            for (int i{ 1 }; i < nums.size(); ++i)
+                if (nums[i] < nums[i - 1])
+                    return false;
+            return true;
+        }
+        else {
+            for (int i{ 1 }; i < nums.size(); ++i)
+            if (nums[i] > nums[i - 1])
+                return false;
+            return true;
+        }
+    }
+};
