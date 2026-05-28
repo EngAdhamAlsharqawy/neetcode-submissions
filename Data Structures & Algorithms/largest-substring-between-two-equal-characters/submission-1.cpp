16db9101f79dd919b82ff70c92052569de441ef7
Adham Al-Sharqawy <eng.adham.alsharqawy@gmail.com>
2026-05-29 02:08:26 +0300

    Add: largest-substring-between-two-equal-characters - submission-1

---
+++ Data Structures & Algorithms/largest-substring-between-two-equal-characters/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int maxLength{ -1 };
+        for (int i{}; i < s.size(); ++i)
+            for (int j{ i + 1 }; j < s.size(); ++j)
+                if (s[i] == s[j])
+                    maxLength = max(maxLength, j - i - 1);
+        return maxLength;
+    }
+};
