add6d28b8d5b9622fd6d62b15bc81b020a061d96
Adham Al-Sharqawy <eng.adham.alsharqawy@gmail.com>
2026-05-31 02:58:18 +0300

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int servedStudents{};
+        int totalStudents = students.size();
+        for (int studentIndex{}, sandwichIndex{}, consecutiveRefusals{};
+             consecutiveRefusals < totalStudents - servedStudents &&
+             sandwichIndex < sandwiches.size(); ++studentIndex) {
+            if (students[studentIndex] == sandwiches[sandwichIndex])
+                ++servedStudents, ++sandwichIndex, consecutiveRefusals = 0;
+            else
+                students.push_back(students[studentIndex]),
+                    ++consecutiveRefusals;
+        }
+        return totalStudents - servedStudents;
+    }
+};
