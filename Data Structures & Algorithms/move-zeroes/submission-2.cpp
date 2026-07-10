a28a0d9141c5c91a88e7b6febfe18acac078a716
Adham Al-Sharqawy <eng.adham.alsharqawy@gmail.com>
2026-07-11 01:44:31 +0300

    Add: move-zeroes - submission-2

---
+++ Data Structures & Algorithms/move-zeroes/submission-2.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int i1{}, i2{}; i2 < nums.size(); ++i2)
+            if (nums[i2])
+                swap(nums[i1++], nums[i2]);
+    }
+};
